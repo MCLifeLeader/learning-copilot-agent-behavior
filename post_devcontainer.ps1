01c480d0683b852a4eca73dc894958eb08e0c1c7
Michael Carey <kb7ppb@ChurchOfJesusChrist.org>
2025-05-30 23:22:03 -0600

    Add Docker setup for SQL Server and initial database schema
    - Created docker-compose.yml to define SQL Server service.
    - Added Dockerfile for SQL Server with necessary configurations.
    - Implemented db-init.sh to initialize the database with schema and data.
    - Added db-init.sql for creating tables and inserting initial data.
    - Created entrypoint.sh to manage the startup process of SQL Server.
    - Introduced service-bus configuration for message queuing.
    - Set up Azure DevOps pipeline for automated builds and releases.
    - Added release notes template for tracking changes.
    - Implemented PowerShell scripts for Docker setup and teardown.
    - Configured development environment setup scripts for project initialization.

---
+++ post_devcontainer.ps1
@@ -0,0 +1,23 @@
+# Setup Development Environment DevContainer
+Write-Host "Post Create Commands for Environment..."
+
+# Update the system
+sudo apt update
+sudo apt upgrade -y
+
+dotnet workload update
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+
+# Setup git Configurations
+git config --global credential.useHttpPath true
+git config --global core.autocrlf true
+git config --global core.eol native
+
+# Install Package Manager Support
+sudo apt install -y nuget
+# sudo apt install -y npm
+
+# Trust HTTPS developer certificate
+dotnet dev-certs https --trust
+
+Write-Host "run  Setup_project.ps1  to complete the process..."
